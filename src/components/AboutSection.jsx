13ab7055e578fa61b67f4bf9b4a06826baf94131
GUEDDARI WASSIM <wassimgueddari13@gmail.com>
2025-09-10 13:02:44 +0100

    optimizing

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -11,7 +11,7 @@ export default function AboutSection() {
 
             <div className='grid grid-cols-1 md:grid-cols-2 gap-12 items-center'>
                 <div className='space-y-6'>
-                    <h3 className='text-2xl font-semibold'>Passionate web/mobile Developer</h3>
+                    <h3 className='text-2xl font-semibold'>Full Stack Developer</h3>
                     <p className='text-muted-foreground'>
                         I specialize in creating responsive,accessible,and performant
                         web/mobile applications using modern technologies.
